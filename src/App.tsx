0a12e5457f8ab9b6ba3be8a2cae821e2b9a9c8c3
sergo <nakato1995@gmail.com>
2021-08-20 20:13:34 +0400

    home commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,13 @@ import React from 'react';
 import styled from 'styled-components';
 import {NewNoteInput} from './Components/NewNoteInput';
 import {useRootStore} from './RootStateContext';
-import CloudIcon from '@material-ui/icons/Cloud';
-// import {NotesStore} from './store/NotesStore';
-// import {inject} from "mobx-react";
-
-
-// type RemoveWeatherProps = {
-//     removeWeather: NotesStore["removeCity"]
-// }
+// import CloudIcon from '@material-ui/icons/Cloud';
+import {WeatherCard} from "./Components/WeatherCard";
 
 const App: React.FC = observer(() => {
     const {notesStore} = useRootStore();
     console.log(notesStore.weathersForcastArr);
 
-    const deleteCityHandler = (event: string) => {
-        console.log(event)
-        // removeWeather(event)
-    }
 
     return (
         <>
@@ -29,41 +19,18 @@ const App: React.FC = observer(() => {
                 <ul>
                     {
                         notesStore.weathers.map((note: any) => (
-                            <WeatherFieldContainer>
-                                <CityName key={note.id}>
-                                    <CloudIcon/>
-                                    {note.name}
-                                </CityName>
-                                <WeatherField className="">
-                                    <p>Temperature °K : {note.main.temp}</p>
-                                </WeatherField>
-
-                                <WeatherField>
-                                    <p>Clouds : {note.weather[0].description}</p>
-                                </WeatherField>
-
-                                <WeatherField className="">
-                                    <p>Humidity : {note.main.humidity}</p>
-                                </WeatherField>
-
-                                <WeatherField className="">
-                                    <p>Pressure : {note.main.pressure}</p>
-                                </WeatherField>
-
-                                <WeatherField className="">
-                                    <p>Country : "{note.sys.country}"</p>
-                                </WeatherField>
-
-                                <WeatherField className="">
-                                    <p>Wind deg : {note.wind.deg}</p>
-                                </WeatherField>
-
-                                <WeatherField className="">
-                                    <p>Wind speed : {note.wind.speed}</p>
-                                </WeatherField>
-
-                                <button onClick={() => deleteCityHandler(note.name)}>Delete</button>
-                            </WeatherFieldContainer>
+                            <WeatherCard
+                                removeWeather={notesStore.removeCity}
+                                id={note.id}
+                                name={note.name}
+                                temp={note.main.temp}
+                                description={note.weather[0].description}
+                                humidity={note.main.humidity}
+                                pressure={note.main.pressure}
+                                country={note.sys.country}
+                                deg={note.wind.deg}
+                                speed={note.wind.speed}
+                            />
                         ))
                     }
                 </ul>
@@ -84,29 +51,3 @@ const MainPageContainer = styled.div`
 
 `;
 
-const WeatherFieldContainer = styled.div`
-  margin-top: 20px;
-  padding: 35px;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  transition: 0.3s;
-  width: 250px;
-  border-radius: 15px;
-
-  &:hover {
-    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
-  }
-`;
-
-const WeatherField = styled.div`
-  display: flex;
-  /* justify-content: space-between; */
-  align-items: center;
-  margin: 5px;
-`;
-
-const CityName = styled.h2`
-  display: flex;
-  justify-content: space-between;
-  color: crimson;
-  align-items: center;
-`;
